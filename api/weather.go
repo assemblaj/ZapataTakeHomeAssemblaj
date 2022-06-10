1092060db985514e22fc424a301234a6f64acd7b
assemblaj <7599535+assemblaj@users.noreply.github.com>
2022-06-10 16:57:59 -0400

    Update weather.go

--- api/weather.go
+++ api/weather.go
@@ -11,21 +11,6 @@ import (
 type WeatherHandler struct{}
 
 func (w WeatherHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	/*
-		fileName := path.Base(r.URL.Path)
-		//api(rootPage + fileName)
-		url := utils.RootPage + fileName
-		response, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(rw, "404 Page not found")
-		} else {
-			defer response.Body.Close()
-			body, err2 := io.ReadAll(response.Body)
-			if err2 != nil {
-				log.Fatal(err)
-			}
-			fmt.Fprintf(rw, string(body))
-		} */
 	res, err := utils.GetPageData(rw, r)
 	if err != nil {
 		log.Fatal(err)
